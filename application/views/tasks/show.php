075fe5fd17ee6c46ac2146c8607c0a850eee5e3c
Ankur Patel <ankur2194@gmail.com>
2019-05-10 18:42:21 +0530

    Tasks Module - Improove Placeholder Instruction

--- application/views/tasks/show.php
+++ application/views/tasks/show.php
@@ -46,7 +46,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label>Your Note</label>
-                                            <textarea class="form-control" name="note" placeholder="Your Note" rows="10" ctrl-enter-submit></textarea>
+                                            <textarea class="form-control" name="note" placeholder="Your Note (You can use Ctrl + Enter for Submit)" rows="10" ctrl-enter-submit></textarea>
                                         </div>
                                     </div>
                                 </div>
