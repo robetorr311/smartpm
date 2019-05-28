f345f61174b127e856b30a4162ddde6c18c96893
Meena-developer <34933393+Meena-developer@users.noreply.github.com>
2019-05-28 17:24:55 +0530

    <Remove old auth file and function>

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -63,23 +63,6 @@ class Dashboard extends CI_Controller {
 		$this->load->view('add_doc', $data);
 		$this->load->view('footer');
 	}
-	public function logout(){
-		$this->session->sess_destroy();
-		foreach($_SESSION as $keys => $values) {
-		  unset($_SESSION[$keys]);
-		}
-		foreach($_COOKIE as $key=>$value) {
-		  setcookie($key,"",1);
-		}
-		
-		delete_cookie('tokenCookie');
-		delete_cookie('csrf_cookie_name');
-		$cookie_name = 'tokenCookie';
-		unset($_COOKIE[$cookie_name]);
-		setcookie($cookie_name, FALSE, time()-10, '/');
-		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate, no-transform, max-age=0, post-check=0, pre-check=0");
-        $this->output->set_header("Pragma: no-cache");
-		redirect('/','refresh');
-    }
+	
 
 }
