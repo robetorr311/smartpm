48e073ac0753d37c52d1e32d9f07f276102b171b
Ankur Patel <ankur2194@gmail.com>
2019-06-18 15:23:13 +0530

    DBS - DB selection Helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] =  array('url', 'file', 'cookie', 'auth','form','security','cookie');
+$autoload['helper'] =  array('url', 'file', 'cookie', 'auth','form','security','db');
 
 /*
 | -------------------------------------------------------------------
