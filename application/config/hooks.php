9d72ec99ce4de643c2462834c8e9bf7637d85fc1
Ankur Patel <ankur2194@gmail.com>
2019-06-18 16:26:47 +0530

    DBS - Hook activated

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -11,3 +11,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['post_controller_constructor'][] = [
+    'class' => 'DbSelection',
+    'function' => 'selectDB',
+    'filename' => 'DbSelection.php',
+    'filepath' => 'hooks'
+];
