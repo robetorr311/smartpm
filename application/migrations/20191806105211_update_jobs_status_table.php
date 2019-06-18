aaf0e8ef9d92568983b772d51382bd4e6d91785a
Meena-developer <34933393+Meena-developer@users.noreply.github.com>
2019-06-18 11:03:12 +0530

    mig table

---
+++ application/migrations/20191806105211_update_jobs_status_table.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Update_jobs_status_table extends CI_Migration
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->dbforge();
+    }
+
+    public function up()
+    {
+        $fields = [
+            'start_at' => [
+                'type' => 'DATETIME'
+            ],
+            'close_at' => [
+                'type' => 'DATETIME'
+            ],
+        ];
+
+        $this->dbforge->modify_column('jobs_status', $fields);
+    }
+
+    public function down()
+    {
+        $fields = [
+            'start_at' => [
+                'type' => 'DATETIME'
+            ],
+            'close_at' => [
+                'type' => 'DATETIME'
+            ],
+        ];
+        $this->dbforge->modify_column('jobs_status', $fields);
+    }
+}
