162d51198d8308e597fed74501f383166b398a34
Web Developer <websitedesignanddeveloper@gmail.com>
2020-04-28 18:39:21 +0530

    Camera button

--- application/views/leads/show.php
+++ application/views/leads/show.php
@@ -40,7 +40,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
                 </div>
                 <div class="footer">
                     <div class="row">
+                        <div class="col-md-12 camera-files-status">
+                            <div>Files uploaded succfully !!</div>
+                        </div>
                         <div class="col-md-12">
+                            <label class="btn btn-fill camera-button">Camera<input type="file" accept="image/*" capture="camera" id="camera-uploads" style="display: none;" multiple /></label>
                             <a href="<?= base_url('lead/' . $lead->id . '/photos'); ?>" class="btn btn-fill">Photos</a>
                             <a href="<?= base_url('lead/' . $lead->id . '/reports'); ?>" class="btn btn-fill">Photo Report</a>
                             <a href="<?= base_url('lead/' . $lead->id . '/docs'); ?>" class="btn btn-fill">Docs</a>
@@ -173,7 +177,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
                         <div class="col-md-12">
                             <div class="form-group">
                                 <label>Address<span class="red-mark">*</span></label>
-                                <input class="form-control" placeholder="Address" name="address" value="<?= $lead->address ?>" type="text">
+                                <input class="form-control" placeholder="Address" name="address" value="<?= $lead->email ?>" type="text">
                             </div>
                         </div>
                     </div>
@@ -543,5 +547,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
         </div>
     </div>
 </div>
-
+<script type="text/javascript">
+    var baseUrl = '<?= base_url(); ?>';
+    var jobid = "<?php echo $jobid; ?>";
+</script>
 <script src="<?= base_url('assets/js/leads/edit.js') ?>"></script>
--- assets/js/leads/edit.js
+++ assets/js/leads/edit.js
@@ -53,3 +53,54 @@ form.addEventListener("submit", function (e) {
         displayValidationError(errors);
     }
 });
+$('#camera-uploads').change(function (){
+    var file_data = $(this)[0].files;
+    /*var len_files = file_data.length;
+    var form_data = new FormData();
+    for (var i = 0; i < len_files; i++) {
+        form_data.append("photo[]", file_data[i]);
+    }*/
+    var len_files = file_data.length;
+    for (var i = 0; i < len_files; i++) {
+        var form_data = new FormData();
+        form_data.append("photo[]", file_data[i]);
+        $.ajax({
+          url: baseUrl + 'lead/'+ jobid +'/photo/upload', // point to server-side PHP script     
+          dataType: 'text', // what to expect back from the PHP script, if anything
+          cache: false,
+          contentType: false,
+          processData: false,
+          data: form_data,
+          type: 'post',
+          success: function(php_script_response) {
+            var obj = JSON.parse(php_script_response)
+            if (obj.img && obj.img.length != 0) {
+              $.ajax({
+                type: 'POST',
+                url: baseUrl + 'lead/'+ jobid +'/photo/save', // point to server-side PHP script     
+                data: {
+                  id: jobid,
+                  name: JSON.stringify(obj.img)
+                },
+                success: function(photoid) {
+                  //alert(photoid);
+                  console.log("DDDDD")
+                  $('.camera-files-status div').show();
+                }
+              });
+            } else if (obj.error) {
+              alert(obj.error);
+            } else {
+              alert('Something went wrong!. File type not ok');
+            }
+          },
+          error: function(jqXHR) {
+            if (jqXHR.status == 413) {
+              alert('Large File, Max file size limit is 100MB.');
+            } else {
+              alert('Something went wrong!. File type not ok');
+            }
+          }
+        });
+    }
+});
