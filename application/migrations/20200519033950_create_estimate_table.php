45e329030a8f5e6851aeeeb0e69e47e73a4531f0
websitedesignanddeveloper <websitedesignanddeveloper@gmail.com>
2020-06-02 11:19:29 +0530

    migration files new

---
+++ application/migrations/20200519033950_create_estimate_table.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Create_estimate_table extends CI_Migration
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->dbforge();
+    }
+    public function up()
+    {
+        $field = [
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE
+            ],
+            'estimate_no' => [
+               'type' => 'INT',
+               'constraint' => 11
+           ],
+            'client_id' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'item_id' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'estimate_Date' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'total' => [
+                'type' => 'INT',
+                'constraint' => 11
+            ],
+            'balance_due' => [
+                'type' => 'INT',
+                'constraint' => 11
+           ],
+            'estimate_total' => [
+                'type' => 'INT',
+                'constraint' => 11
+           ],
+            'created_at TIMESTAMP default CURRENT_TIMESTAMP',
+            'updated_at TIMESTAMP default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+        ];
+        $this->dbforge->add_field($field);
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('estimate', TRUE);
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('estimate', TRUE);
+    }
+}
