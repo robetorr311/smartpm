7284b05bfb056dd62582671e0c1eba633a10002b
unknown <you@example.com>
2020-09-05 15:37:22 +0530

    Mailtrap condition is added for only localhost

--- application/libraries/Notify.php
+++ application/libraries/Notify.php
@@ -28,26 +28,31 @@ class Notify
             }
         }
 
-        // $this->CI->email->initialize([
-        //     'protocol' => 'smtp',
-        //     'smtp_crypto' => (isset($smtpSettings) && !empty($smtpSettings->smtp_host)) ? $smtpSettings->smtp_crypto : getenv('EMAIL_SMTP_CRYPTO'),
-        //     'smtp_host' => (isset($smtpSettings) && !empty($smtpSettings->smtp_host)) ? $smtpSettings->smtp_host : getenv('EMAIL_SMTP_HOST'),
-        //     'smtp_port' => intval((isset($smtpSettings) && $smtpSettings->smtp_port != 0) ? $smtpSettings->smtp_port : getenv('EMAIL_SMTP_PORT')),
-        //     'smtp_user' => (isset($smtpSettings) && !empty($smtpSettings->smtp_user)) ? $smtpSettings->smtp_user : getenv('EMAIL_SMTP_USER'),
-        //     'smtp_pass' => (isset($smtpSettings) && !empty($smtpSettings->smtp_pass)) ? $smtpSettings->smtp_pass : getenv('EMAIL_SMTP_PASS'),
-        //     'mailtype' => 'html'
-        // ]);
+        if($_SERVER['HTTP_HOST'] == 'localhost') {
+            // For mailtrap
+            $this->CI->email->initialize([
+                'protocol' => 'smtp',
+                'smtp_crypto' => getenv('EMAIL_SMTP_CRYPTO'),
+                'smtp_host' => getenv('EMAIL_SMTP_HOST'),
+                'smtp_port' => getenv('EMAIL_SMTP_PORT'),
+                'smtp_user' => getenv('EMAIL_SMTP_USER'),
+                'smtp_pass' => getenv('EMAIL_SMTP_PASS'),
+                'mailtype' => 'html'
+            ]);
+        } else {
+            $this->CI->email->initialize([
+                'protocol' => 'smtp',
+                'smtp_crypto' => (isset($smtpSettings) && !empty($smtpSettings->smtp_host)) ? $smtpSettings->smtp_crypto : getenv('EMAIL_SMTP_CRYPTO'),
+                'smtp_host' => (isset($smtpSettings) && !empty($smtpSettings->smtp_host)) ? $smtpSettings->smtp_host : getenv('EMAIL_SMTP_HOST'),
+                'smtp_port' => intval((isset($smtpSettings) && $smtpSettings->smtp_port != 0) ? $smtpSettings->smtp_port : getenv('EMAIL_SMTP_PORT')),
+                'smtp_user' => (isset($smtpSettings) && !empty($smtpSettings->smtp_user)) ? $smtpSettings->smtp_user : getenv('EMAIL_SMTP_USER'),
+                'smtp_pass' => (isset($smtpSettings) && !empty($smtpSettings->smtp_pass)) ? $smtpSettings->smtp_pass : getenv('EMAIL_SMTP_PASS'),
+                'mailtype' => 'html'
+            ]);
+        }
+        
 
-        // For mailtrap
-        $this->CI->email->initialize([
-            'protocol' => 'smtp',
-            'smtp_crypto' => getenv('EMAIL_SMTP_CRYPTO'),
-            'smtp_host' => getenv('EMAIL_SMTP_HOST'),
-            'smtp_port' => getenv('EMAIL_SMTP_PORT'),
-            'smtp_user' => getenv('EMAIL_SMTP_USER'),
-            'smtp_pass' => getenv('EMAIL_SMTP_PASS'),
-            'mailtype' => 'html'
-        ]);
+        
         $this->CI->email->set_newline("\r\n");
         $this->CI->email->from(((isset($smtpSettings) && !empty($smtpSettings->smtp_user)) ? $smtpSettings->smtp_user : getenv('EMAIL_SMTP_USER')), 'SmartPM Notification');
     }
